fe30136f036bd63f1ab95f0e6ca0bc9ad3925d36
prakash shukla <prakashshukla275@gmail.com>
2021-11-20 16:44:00 +0530

    creating function add two numbers

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,13 @@
  const defaultResult = 0;
 let currentResult =  defaultResult;
 
+ const add = ( num1 , num2 ) =>{
+         const result = num1 + num2;
+         alert()
+ }
+
 currentResult = (currentResult + 10) * 3 / 2 - 1 ;
 
-let calculationDescrip = '('+defaultResult+'+ 10) * 3 /2 - 1 '
+let calculationDescrip = `${defaultResult} + 10 * 3 /2 - 1 `;
 
 outputResult(currentResult, calculationDescrip);
