516c6ff19d68090a07b6b68fa538a68006e7c5a8
prakash shukla <prakashshukla275@gmail.com>
2021-11-20 15:50:21 +0530

    create new object

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,8 @@
-let currentResult = 0;
+ const defaultResult = 0;
+let currentResult =  defaultResult;
 
-currentResult = currentResult +10;
+currentResult = (currentResult + 10) * 3 / 2 - 1 ;
 
-let calculationDescrip
+let calculationDescrip = '('+defaultResult+'+ 10) * 3 /2 - 1 '
 
-outputResult(currentResult, '');
+outputResult(currentResult, calculationDescrip);
