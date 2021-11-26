dd0d1d792ece77de521f4d2f7ab46abf3867274d
prakash shukla <prakashshukla275@gmail.com>
2021-11-26 15:01:18 +0530

    add some code

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -14,6 +14,25 @@ let currentResult =  defaultResult;
           currentResult = currentResult + enteredNumber ;
           outputResult(currentResult,calcDescription);
  }
+
+   const subtract = () =>{
+    const enteredNumber = getUserNumberInput();
+    const  calcDescription = `${currentResult} - ${enteredNumber}`
+        currentResult = currentResult - enteredNumber ;
+        outputResult(currentResult,calcDescription);
+   }
+
+    const multiply = () =>{
+        const enteredNumber = getUserNumberInput();
+        const  calcDescription = `${currentResult} + ${enteredNumber}`
+            currentResult = currentResult + enteredNumber ;
+            outputResult(currentResult,calcDescription);
+
+    }
+
+    const divide = () =>{
+
+    }
     
  addBtn.addEventListener('click',add);
  
